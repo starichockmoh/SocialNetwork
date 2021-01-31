56fb5453e69491517ca21dff239b53e755a39eca
starichockmoh <an.yan.7210@gmail.com>
2021-01-31 21:08:09 +0400

    Updates

---
+++ precache-manifest.9356b10c3de6830e0774c820939007d9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743911fe8151160fc48a693babe13975",
+    "url": "/FirstProject/index.html"
+  },
+  {
+    "revision": "99ac2cd817ebb74740ea",
+    "url": "/FirstProject/static/css/3.3e602a44.chunk.css"
+  },
+  {
+    "revision": "3e3459c9d3c1db7d186e",
+    "url": "/FirstProject/static/css/4.4999de9d.chunk.css"
+  },
+  {
+    "revision": "7b83c7342c9415900e95",
+    "url": "/FirstProject/static/css/5.2b283795.chunk.css"
+  },
+  {
+    "revision": "0b206daf2dab64b3ab57",
+    "url": "/FirstProject/static/css/6.0ab0ac3b.chunk.css"
+  },
+  {
+    "revision": "3d78c42023ab62d96242",
+    "url": "/FirstProject/static/css/main.b429edb8.chunk.css"
+  },
+  {
+    "revision": "f985705dd1d1f1d391c6",
+    "url": "/FirstProject/static/js/2.41cffa13.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/FirstProject/static/js/2.41cffa13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ac2cd817ebb74740ea",
+    "url": "/FirstProject/static/js/3.717efb4c.chunk.js"
+  },
+  {
+    "revision": "3e3459c9d3c1db7d186e",
+    "url": "/FirstProject/static/js/4.285fbfff.chunk.js"
+  },
+  {
+    "revision": "7b83c7342c9415900e95",
+    "url": "/FirstProject/static/js/5.bc150265.chunk.js"
+  },
+  {
+    "revision": "0b206daf2dab64b3ab57",
+    "url": "/FirstProject/static/js/6.336e716e.chunk.js"
+  },
+  {
+    "revision": "9ec7df8c7d66d1d310e0",
+    "url": "/FirstProject/static/js/7.512fdc1a.chunk.js"
+  },
+  {
+    "revision": "b4274f141f4621e08298",
+    "url": "/FirstProject/static/js/8.f91a8346.chunk.js"
+  },
+  {
+    "revision": "3d78c42023ab62d96242",
+    "url": "/FirstProject/static/js/main.1b53019f.chunk.js"
+  },
+  {
+    "revision": "a657612e157c2a1f7d35",
+    "url": "/FirstProject/static/js/runtime-main.e5e87a11.js"
+  },
+  {
+    "revision": "71c1f79803e5ed56eaa5377e2b83b0d7",
+    "url": "/FirstProject/static/media/Bean Eater-1s-200px.71c1f798.svg"
+  },
+  {
+    "revision": "79eb8ca50778037dfc17795f614a3251",
+    "url": "/FirstProject/static/media/computer-icons-user-profile-avatar-png-favpng-CXDB2aUAq6zHS7pQSY9GjQ3ZH.79eb8ca5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FirstProject/precache-manifest.ccd88572f108184c5b5a7e2290334a7c.js"
+  "/FirstProject/precache-manifest.9356b10c3de6830e0774c820939007d9.js"
 );
 
 self.addEventListener('message', (event) => {
